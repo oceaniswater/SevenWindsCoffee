82115e1840720a2e83776871e8e2f44b4d9b840a
oceaniswater <markflyingbear@gmail.com>
2024-02-12 15:35:26 +0000

    add custom appearance, add a separator to map view, change the nav title on map view

--- SevenWindsCoffee/App/SceneDelegate.swift
+++ SevenWindsCoffee/App/SceneDelegate.swift
@@ -16,7 +16,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         guard let windowScene = (scene as? UIWindowScene) else { return }
         
         let registrationRouter = RegistrationRouter.start()
-        
         let initialVC = registrationRouter.entryPoint
         
         let window = UIWindow(windowScene: windowScene)
